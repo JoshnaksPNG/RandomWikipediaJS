3a96e15bc19f7f683f1abdaddbe3d7e02acafc44
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2024-01-10 16:59:38 -0800

    Added BASE_URL and DayAfterTomorrow/Tomorrow Functions

--- src/functions.ts
+++ src/functions.ts
@@ -3,6 +3,7 @@ import { JSDOM } from "jsdom";
 export const RAND_URL : string = "https://en.wikipedia.org/wiki/Special%3ARandom";
 export const NEW_URL : string = "https://en.wikipedia.org/wiki/Special%3ANewPages";
 export const TODAY_URL : string = "https://en.wikipedia.org/wiki/Wikipedia:Today%27s_featured_article";
+export const BASE_URL : string = "https://en.wikipedia.org"
 
 export async function getRandomDocument() : Promise<Document> 
 {
@@ -33,7 +34,7 @@ export async function getNewestURL() : Promise<string>
         {
             if(href)
             {
-                resolve(href);
+                resolve(BASE_URL + href);
             } else
             {
                 resolve("");
@@ -84,7 +85,7 @@ export async function getTodayURL() : Promise<string>
         {
             if(href)
             {
-                resolve(href);
+                resolve(BASE_URL + href);
             } else
             {
                 resolve("");
@@ -126,6 +127,70 @@ export async function getTodayTitle() : Promise<string>
     let title : Promise<string> = new Promise<string>((resolve, reject) => 
     {
         getTodayDoc()
+        .then(doc => resolve(doc.title));
+    });
+
+    return title;
+}
+
+export async function getTomorrowURL(dayAfterTomorrow? : boolean) : Promise<string>
+{
+    let ubermorgen : number;
+
+    if(dayAfterTomorrow)
+    {
+        ubermorgen = 1;
+    } else
+    {
+        ubermorgen = 0;
+    }
+    
+
+    let document : Promise<string> = new Promise<string>((resolve, reject) =>
+    {
+        fetch(TODAY_URL)
+        .then(res => res.text())
+        .then(html => new JSDOM(html).window.document)
+        .then(doc => doc.querySelectorAll(".MainPageBG"))
+        .then(elemcollection => elemcollection.item(ubermorgen + 1))
+        .then(element => element?.getElementsByTagName("b"))
+        .then(bcollection => bcollection?.item(0))
+        .then(firstb => firstb?.getElementsByTagName("a"))
+        .then(acollection => acollection?.item(0))
+        .then(link => link?.getAttribute("href"))
+        .then(href => 
+        {
+            if(href)
+            {
+                resolve(BASE_URL + href);
+            } else
+            {
+                resolve("");
+                throw new Error("URL Not Found");
+            }
+        })
+        .catch( e =>
+        {
+            resolve("");
+            throw new Error("URL Not Found\n\n" + e);
+        });
+    });
+
+    return document;
+}
+
+export async function getTomorrowDoc(dayAfterTomorrow? : boolean) : Promise<Document>
+{
+    let url : string = await getTomorrowURL(dayAfterTomorrow);
+    
+    return getDocByURL(url);
+}
+
+export async function getTomorrowTitle(dayAfterTomorrow? : boolean) : Promise<string>
+{
+    let title : Promise<string> = new Promise<string>((resolve, reject) => 
+    {
+        getTomorrowDoc(dayAfterTomorrow)
         .then(doc => resolve(doc.title));
     });
 
